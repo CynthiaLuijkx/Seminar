cc3a9d9b956a10b7d0979e09d3e7a252b006f76f
CynthiaLuijkx <c.luijkx@planet.nl>
2020-02-13 15:36:33 +0100

    Updated all Tools classes

--- src/Tools/Node.java
+++ src/Tools/Node.java
@@ -1,17 +1,22 @@
 package Tools;
 
+/**
+ * This method corresponds to a node in the directed graph containing the day number and the duty number.
+ * @author Mette Wagenvoort
+ *
+ */
 public class Node 
 {
-	private final int day;
+	private final int dayNr;
 	private final int dutyNr;
 	
-	public Node(int day, int dutyNr) {
-		this.day = day;
+	public Node(int dayNr, int dutyNr) {
+		this.dayNr = dayNr;
 		this.dutyNr = dutyNr;
 	}
 
-	public int getDay() {
-		return day;
+	public int getDayNr() {
+		return dayNr;
 	}
 
 	public int getDutyNr() {
@@ -20,6 +25,6 @@ public class Node
 
 	@Override
 	public String toString() {
-		return "Node [day=" + day + ", dutyNr=" + dutyNr + "]";
+		return "Node [dayNr=" + dayNr + ", dutyNr=" + dutyNr + "]";
 	}
 }
